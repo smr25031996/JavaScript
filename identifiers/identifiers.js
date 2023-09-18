d12b51f8986b8136e70ac87d0030c14747198dc1
Shubham Rajput <rajputshubham615@gmail.com>
2023-09-18 12:19:27 +0530

    learnt Identifiers

---
+++ identifiers/identifiers.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line linebreak-style
+/* eslint-disable prefer-const */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+let name='Shubham';
+console.log('welcome to JS course '+name);
+// let name='Shubham';
+// console.log('welcome to JS course '+name);
+
+const pi=3.14;
+console.log('pi'+pi);
+// const pi=3.124;
+
