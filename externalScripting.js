729ee53f703c7b7b1aaee543e3a28fecfd55cd0d
Shubham Rajput <rajputshubham615@gmail.com>
2023-09-18 11:58:24 +0530

    first commit

---
+++ externalScripting.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+document.getElementById('name').innerHTML = 'shubham';
+
+// eslint-disable-next-line linebreak-style, no-unused-vars
+const firstName = 'Rexha';
+// eslint-disable-next-line linebreak-style
+// eslint-disable-next-line no-unused-vars
+const lastName = 'Bebe';
+let result;
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+  result=i;
+  document.getElementById('table').innerHTML=`<li>${result}</li>`;
+}
